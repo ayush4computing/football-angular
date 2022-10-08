9a2e881b1f604a5afee01720b65da43cf97c86b1
ayush4computing <ayush.agarwal6530@gmail.com>
2022-10-08 15:48:50 +0000

    ommit

--- src/app/components/match-detail/match-detail.component.spec.ts
+++ src/app/components/match-detail/match-detail.component.spec.ts
@@ -5,6 +5,7 @@ import { BrowserModule, By } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from "@angular/router/testing";
 
 describe('MatchDetailComponent', () => {
   let players: Player[];
@@ -14,7 +15,8 @@ describe('MatchDetailComponent', () => {
       imports: [
         ReactiveFormsModule,
         BrowserModule,
-        HttpClientModule
+        HttpClientModule,
+        RouterTestingModule
       ],
       declarations: [
         MatchDetailComponent,
